f31730f063abe12928a77fa905a177c37e502514
thomas/NERO <tomconmo@gmail.com>
2018-08-03 23:44:31 -0400

    CharacterList placeholder component

--- web/app/modules/app/home.tsx
+++ web/app/modules/app/home.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import CharacterForm from "web/app/modules/character/character-form";
+import CharacterList from "web/app/modules/character/character-list";
 
 export const Home = () => {
       return <div>
          <CharacterForm />
+         <CharacterList characters={[]} />
       </div>;
    }
