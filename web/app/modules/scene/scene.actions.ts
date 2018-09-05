5cb7e65c99f97ee29a47ee19ce2299d0bade4269
thomascmost <thomas@abacus.com>
2018-09-05 01:23:51 -0700

    filling up a matrix

--- web/app/modules/scene/scene.actions.ts
+++ web/app/modules/scene/scene.actions.ts
@@ -1,5 +1,8 @@
 export const GET_SCENES_REQUEST = "GET_SCENES_REQUEST";
 export const GET_SCENES_SUCCESS = "GET_SCENES_SUCCESS";
+export const ADD_SCENE_REQUEST = "ADD_SCENE_REQUEST";
+export const ADD_SCENE_SUCCESS = "ADD_SCENE_SUCCESS";
+
 
 export const getScenes = () => {
    return {
@@ -13,3 +16,18 @@ export const getScenesSuccess = (scenes: any[]) => {
       payload: scenes
    };
 };
+
+
+export const addScene = (x: number, y: number) => {
+   return {
+      type: ADD_SCENE_REQUEST,
+      payload: {x, y}
+   };
+};
+
+export const addSceneSuccess = (scenes: any[]) => {
+   return {
+      type: ADD_SCENE_SUCCESS
+   };
+};
+
--- web/app/modules/scene/scene.reducer.ts
+++ web/app/modules/scene/scene.reducer.ts
@@ -1,6 +1,6 @@
 
 import { SceneState } from "./scene.state";
-import { GET_SCENES_SUCCESS } from "./scene.actions";
+import { GET_SCENES_SUCCESS, ADD_SCENE_REQUEST } from "./scene.actions";
 
 export function sceneReducer(state: SceneState = new SceneState(), action: any): SceneState {
    switch (action.type)
@@ -8,6 +8,20 @@ export function sceneReducer(state: SceneState = new SceneState(), action: any):
       case GET_SCENES_SUCCESS:
       {
          return {...state, list: action.payload};
+      }
+      case ADD_SCENE_REQUEST:
+      {
+         let {x,y} = action.payload;
+         let list = state.list.slice();
+         list.push({
+            name: "New Scene",
+            id: Math.random().toString(),
+            gridX: x,
+            gridY: y,
+            lengthGrid: 2,
+            colSpan: 1
+         })
+         return {...state, list: list};
       }
       default:
       {
